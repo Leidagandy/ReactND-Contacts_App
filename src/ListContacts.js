ec0143d6052929656ef08ad62e0c312fd00fc8bc
Leida_Gandy <leida.gandy@gmail.com>
2021-01-07 18:05:27 +0000

    just testing

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -21,10 +21,8 @@ import React, { Component } from "react";
 //     </ol>
 //   );
 // }
-
+// tedting if this will push to github
 class ListContacts extends Component {
-
-  
   render() {
     // console.log("this is the props", this.props);
     return (
@@ -41,7 +39,12 @@ class ListContacts extends Component {
               <p>{contact.name}</p>
               <p>{contact.handle}</p>
             </div>
-            <button className="contact-remove">Remove</button>
+            <button
+              onClick={() => this.props.onDeleteContact(contact)}
+              className="contact-remove"
+            >
+              Remove
+            </button>
           </li>
         ))}
       </ol>
